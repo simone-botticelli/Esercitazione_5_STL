989cd2ca9d3c42f94a8b5523070aea733f73c2d8
Simone <simone.botticelli1@gmail.com>
2025-04-29 00:22:38 +0200

    initial commit

---
+++ Esercise1_final/src/Utils.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include "TriangularMesh.hpp"
+
+using namespace std;
+
+namespace TriangularLibrary
+{
+/// Import the triangular mesh and test if the mesh is correct
+/// mesh: a TriangularMesh struct
+/// return the result of the reading, true if is success, false otherwise
+bool ImportMesh(TriangularMesh& mesh);
+
+/// Import the Cell0D properties from Cell0Ds.csv file
+/// mesh: a TriangularMesh struct
+/// return the result of the reading, true if is success, false otherwise
+bool ImportCell0Ds(TriangularMesh& mesh);
+
+/// Import the Cell1D properties from Cell1Ds.csv file
+/// mesh: a TriangularMesh struct
+/// return the result of the reading, true if is success, false otherwise
+bool ImportCell1Ds(TriangularMesh& mesh);
+
+/// Import the Cell2D properties from Cell2Ds.csv file
+/// mesh: a TriangularMesh struct
+/// return the result of the reading, true if is success, false otherwise
+bool ImportCell2Ds(TriangularMesh& mesh);
+
+}
